ce99715838cde80928c65cdf890d31d2a8c80755
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-07-22 21:18:33 +0530

    1st day

--- app.js
+++ app.js
@@ -7,5 +7,7 @@ const app = express();
 
 app.use(mainRoutes);
 app.use('/api',apiRoutes);
-
+/*
+https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNYDljtAeI8oZzelFzj7-Tw&order=date&type=video&videoSyndicated=true&key=AIzaSyCg5M3KgYCgQju3c7AMEfaLyQroFx6xzX4
+*/
 app.listen(3000);
