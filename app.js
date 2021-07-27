6e7db6d0d0db61e27e86946c2d287befc33ce201
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-07-27 19:03:26 +0530

    Fixed port

--- app.js
+++ app.js
@@ -15,10 +15,10 @@ app.use(mainRoutes);
 app.use('/api',apiRoutes);
 // Now register the subdomain middleware.
 
-
+const PORT = process.env.PORT | 3000;
 /*
 https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=UCNYDljtAeI8oZzelFzj7-Tw&order=date&type=video&videoSyndicated=true&key=AIzaSyCg5M3KgYCgQju3c7AMEfaLyQroFx6xzX4
 */
-app.listen(3000, ()=> {
-    console.log('Server is started!');
+app.listen(PORT, ()=> {
+    console.log('Server is now live on PORT: '+ PORT);
 });
