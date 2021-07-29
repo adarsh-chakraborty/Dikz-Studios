060192d1b73c935f0b60c262fa49d9d9c3765b56
Adarsh Chakraborty <adarshchak108@gmail.com>
2021-07-29 16:38:04 +0530

    Added Social and Navigation links

--- controllers/main.js
+++ controllers/main.js
@@ -19,13 +19,18 @@ exports.getSocialLink = (req,res,next) => {
             res.redirect('https://links.dikzstudios.gq/spotify');
             return;
         }
+        // Twitter
+        if(social == 't' || social == 'twitter' || social == 'dikzt' || social == 'dikztwitter'){
+            res.redirect('https://links.dikzstudios.gq/twitter');
+            return;
+        }
         // Facebook
         if(social == 'fb' || social == 'facebook' || social == 'dikzfacebook' || social == 'dikzfb'){
             res.redirect('https://links.dikzstudios.gq/facebook');
             return;
         }
-        // Instagram
-        if(social == 'ig' || social == 'insta' || social == 'instagram' || social == 'dikzinsta' || social == 'dikzig' || social == 'dikzinstagram'){
+         // Instagram
+         if(social == 'ig' || social == 'insta' || social == 'instagram' || social == 'dikzinsta' || social == 'dikzig' || social == 'dikzinstagram'){
             res.redirect('https://links.dikzstudios.gq/instagram');
             return;
         }
